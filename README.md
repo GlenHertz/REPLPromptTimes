42ccdb6efa14b43568339e3c19bd7587b918d493
Glen Hertz <glen.hertz@gmail.com>
2022-03-10 22:08:26 +0000

    Add all the code

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # REPLPromptTimes
-Code to display how long the previous command took to run in the prompt
+Enhances the Julia REPL prompt to include the run time of the previous command in the prompt.
+
+## Usage
+
+Don't paste the code into the REPL, but `include` it like so:
+
+```julia
+julia> include("../REPLPromptTimes.jl/prompt_times.jl")
+#7 (generic function with 1 method)
+
+julia [0.227 s]> sleep(5)
+
+julia [5.02 s]>
+```
---
+++ prompt_times.jl
@@ -0,0 +1,37 @@
+# Don't paste into REPL, use `include("prompt_times.jl")`
+
+using REPL
+using Printf
+@eval REPL begin
+     const LAST_TIME = Ref{Float64}(time())
+     const PROMPT_CACHE  = Ref{Union{String, Nothing}}(nothing)
+end
+
+# Format the time printing here as you wish
+function prettytime(t)
+    if REPL.PROMPT_CACHE[] != nothing
+        return REPL.PROMPT_CACHE[]
+    end
+    if t < 1e2
+        value, units = t, "ns"
+    elseif t < 1e5
+        value, units = t / 1e3, "μs"
+    elseif t < 1e8
+        value, units = t / 1e6, "ms"
+    elseif t < 120e9
+        value, units = t / 1e9, "s"
+    elseif t < 1e9*60*90
+	value, units = t / 60e9, "min"
+    else
+	value, units = t / 3600e9, "hr"
+    end
+    s = string(round(value, sigdigits=3), " ", units)
+    REPL.PROMPT_CACHE[] = s
+    return s
+end
+
+
+julia_repl = Base.active_repl.interface.modes[1]
+old_f = julia_repl.on_done
+julia_repl.on_done = (args...) -> (REPL.LAST_TIME[] = time(); REPL.PROMPT_CACHE[] = nothing; old_f(args...))
+julia_repl.prompt = () -> string("julia [", prettytime((time() - REPL.LAST_TIME[])*1e9), "]> ")
